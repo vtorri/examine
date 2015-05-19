2483b1cd6f612371bb7e603de6d1cee5e5e09ce5
Vincent Torri <vincent.torri@gmail.com>
2015-05-19 13:20:27 +0200

    Rename visual studio file

--- src/lib/Makefile.mk
+++ src/lib/Makefile.mk
@@ -38,4 +38,4 @@ src_lib_libexamine_la_LDFLAGS = -no-undefined -version-info @version_info@
 
 src_lib_libexamine_la_LIBTOOLFLAGS = --tag=disable-static
 
-EXTRA_DIST += src/bin/memcheck/examine_stack_vc.c
+EXTRA_DIST += src/bin/memcheck/examine_stack_pdb.c
